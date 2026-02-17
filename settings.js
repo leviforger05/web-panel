57007d43b0674b1d236fbc628ede93ca6cfcc462
Levi Forger <mfaisaliqbal10@gmail.com>
2026-02-17 11:32:46 +0700

    web-panel

---
+++ settings.js
@@ -0,0 +1,67 @@
+/**
+ * alip clutch
+ * do not remove this credit !!!
+ */
+
+module.exports = {
+  // App Config
+  STORE_NAME: "Alip Cloud",
+  CONTACT_WHATSAPP: "https://wa.me/6281249703469",
+
+  // Links Configuration
+  CHANNEL_LINK: "https://whatsapp.com/channel/0029VbBzq5JH5JLtaYovYB0b",
+  BOT_GROUP_LINK: "https://chat.whatsapp.com/ChrOPk4QYToJiFTAA0V7bk?mode=gi_t",
+  STORE_GROUP_LINK: "https://chat.whatsapp.com/ChrOPk4QYToJiFTAA0V7bk?mode=gi_t",
+  CONTACT_ADMIN: "https://wa.me/6281249703469",
+
+  // Website Assets
+  WEBSITE_LOGO: "https://img2.pixhost.to/images/5707/694518103_alip-1770904754646.jpg",
+  WEBSITE_BANNER: "https://img2.pixhost.to/images/5686/694183513_alip-1770827267727.jpg",
+
+  // Admin
+  ADMIN_PASSWORD: "alifalfrlk3c3",
+
+  // Panel Prices (per bulan)
+  PRICES: {
+    PANEL_1GB: 3000,
+    PANEL_2GB: 5000,
+    PANEL_3GB: 7000,
+    PANEL_4GB: 9000,
+    PANEL_5GB: 11000,
+    PANEL_6GB: 14000,
+    PANEL_7GB: 18000,
+    PANEL_PREMIUM: 30000
+  },
+
+  // Payment Gateway
+  PAYMENT: {
+    SLUG: "alif-alfarel",
+    API_KEY: "22Xdnk2uXiGerChxMKMZrWmlldnwM8hp"
+  },
+
+  // System
+  AUTO_DELETE_AFTER_DAYS: 3,
+
+  // GitHub Storage
+  GITHUB: {
+    TOKEN: "gh token",
+    OWNER: "userneme gh",
+    REPO: "web-panel",
+    FILE_PATH: "dataserver.json"
+  },
+
+  // Pterodactyl Config
+  PTERODACTYL: {
+    DOMAIN: "https://",
+    API_KEY: "ptla_",
+    NEST_ID: 5,
+    EGG_ID: 15,
+    LOCATION_ID: 1
+  },
+
+  // Node Server
+  NODE: {
+    HOST: "ip vps",
+    ROOT_PASS: "pw vps"
+  }
+};
